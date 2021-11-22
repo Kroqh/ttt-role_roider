d707542cb80f80156882577f79f5a85e348d7c08
Marcus Krogh <37517255+Kroqh@users.noreply.github.com>
2021-11-22 15:41:58 +0100

    Danish translations

---
+++ lua/terrortown/lang/da/roider.lua
@@ -0,0 +1,14 @@
+L = LANG.GetLanguageTableReference("da")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[ROIDER.name] = "Roider"
+L["info_popup_" .. ROIDER.name] = [[Du er Roideren!
+Du er fortæret af roid raseri, meget stærk men ikke i stand til at sigte ordenligt!]]
+L["info_popup_roider_alone_" .. ROIDER.name] = [[Du er Roideren!
+Du er fortæret af roid raseri, meget stærk men ikke i stand til at sigte ordenligt!]]
+L["body_found_" .. ROIDER.abbr] = "De var en Roider."
+L["search_role_" .. ROIDER.abbr] = "Denne person var en Roider!"
+L["target_" .. ROIDER.name] = "Roider"
+L["ttt2_desc_" .. ROIDER.name] = [[Roideren skal vinde med forræderne]]
+L["label_roider_push_multi"] = "Brækjern skubbeafstand multiplikator"
+L["label_crowbar_dmg"] = "Brækjern skade"
