27eca04f9a470301ed7f8cd8654ff2dbbe177412
Marcus Krogh <37517255+Kroqh@users.noreply.github.com>
2021-11-22 12:58:33 +0100

    RoiderDamageAttempt rename

--- lua/terrortown/autorun/server/sv_roider_handler.lua
+++ lua/terrortown/autorun/server/sv_roider_handler.lua
@@ -1,6 +1,6 @@
 
 
-local function ShouldRoiderDealNoDamage(ply, attacker)
+local function RoiderDamageAttempt(ply, attacker)
 	if not IsValid(ply) or not IsValid(attacker) or not attacker:IsPlayer() or attacker:GetSubRole() ~= ROLE_ROIDER then return end
 	if SpecDM and (ply.IsGhost and ply:IsGhost() or (attacker.IsGhost and attacker:IsGhost())) then return end
 
@@ -8,9 +8,9 @@ local function ShouldRoiderDealNoDamage(ply, attacker)
 end
 
 
-hook.Add("PlayerTakeDamage", "RoiderNoDamage", function(ply, inflictor, killer, amount, dmginfo)
+hook.Add("PlayerTakeDamage", "RoiderDamage", function(ply, inflictor, killer, amount, dmginfo)
 
-    if not ShouldRoiderDealNoDamage(ply, killer) then return end
+    if not RoiderDamageAttempt(ply, killer) then return end
     
     if  inflictor ~= killer and killer:GetActiveWeapon():GetClass() == "weapon_zm_improvised" then
         dmginfo:SetDamage(GetConVar("ttt2_roid_cbdmg"):GetInt())
